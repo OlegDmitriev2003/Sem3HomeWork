65f89eef33726345119cc52c97024dae86c612d4
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-29 10:50:56 +0300

    Finished Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int i = 0;
+bool IsPalindrome = true;
+Console.Write("Введите число: ");
+string Num = Console.ReadLine();
+
+int N = Num.Length;
+
+while (i < N)
+{
+    if (Num[i] == Num[(N-1) - i])
+    {
+i++;
+IsPalindrome = true;
+    }
+
+    else
+    {
+        IsPalindrome = false;
+        i++;
+    }
+}
+
+if (IsPalindrome == true)
+    Console.WriteLine($"Число {Num} - палиндром");
+else
+    Console.WriteLine($"Число {Num} - не палиндром");
