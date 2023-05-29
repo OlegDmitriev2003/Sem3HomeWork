5b8e5ceb6a8ea2b2a0c99680a76ed957c66a846f
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-29 11:27:48 +0300

    Finished Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int i = 0;
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!);
+while (Num >= i)
+    {
+        Console.WriteLine(Math.Pow(i,3));
+        i++;
+    }
